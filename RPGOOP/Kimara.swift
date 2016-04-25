7655d1c815e9938c95578dc3d2ea6db97c73fb2a
Joey <joey@abexworks.com>
2016-04-25 15:03:32 -0400

    first commit

---
+++ RPGOOP/Kimara.swift
@@ -0,0 +1,9 @@
+//
+//  Kimara.swift
+//  RPGOOP
+//
+//  Created by Joseph Waddell on 4/25/16.
+//  Copyright © 2016 Gimmecode. All rights reserved.
+//
+
+import Foundation
---
+++ RPGOOP/enemy.swift
@@ -0,0 +1,9 @@
+//
+//  enemy.swift
+//  RPGOOP
+//
+//  Created by Joseph Waddell on 4/25/16.
+//  Copyright © 2016 Gimmecode. All rights reserved.
+//
+
+import Foundation
